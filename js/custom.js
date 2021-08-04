ac9c466f9086387d8c945fa0372638dc308c4d23
BorisPerkovic <borisperkovic89@yahoo.com>
2021-08-04 23:20:51 +0200

    Vesrion 2.0 completed

--- js/custom.js
+++ js/custom.js
@@ -6,17 +6,6 @@ $(window).load(function(){
 });
 
 
-/* Mobile Navigation
-    -----------------------------------------------*/
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
-
-
 /* HTML document is loaded. DOM is ready. 
 -------------------------------------------*/
 $(document).ready(function() {
@@ -26,23 +15,6 @@ $(document).ready(function() {
     $('.navbar-collapse a').click(function(){
         $(".navbar-collapse").collapse('hide');
     });
-
-
- /* Parallax section
-    -----------------------------------------------*/
-  function initParallax() {
-    $('#about').parallax("100%", 0.3);
-    $('#skills').parallax("100%", 0.2);
-    $('#experience').parallax("100%", 0.3);
-    $('#education').parallax("100%", 0.1);
-    $('#portfolio').parallax("100%", 0.2);
-    $('#register').parallax("100%", 0.1);
-    $('#contact').parallax("100%", 0.2);
-
-  }
-  initParallax();
-
-
 
   /* Back top
   -----------------------------------------------*/
@@ -59,10 +31,5 @@ $(document).ready(function() {
       $('html, body').animate({scrollTop: 0}, 300);
       })
 
-
-  /* wow
-  -------------------------------*/
-  new WOW({ mobile: false }).init();
-
-  });
+});
 
